bcf514683b2029766071ad8e311f5a6cb03dee89
yazan <yazanadel07@gmail.com>
2022-08-06 13:58:25 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+days = int(input("enter days:")) * 3600 * 24
+hours = int(input("enter hours:")) * 3600
+minutes = int(input("enter minutes:")) * 60
+seconds = int(input("enter seconds:"))
+
+times = days + hours + minutes + seconds
+
+print("the total of seconds:" ,times )
